433ae5aeb105ffbe5aa002d2643f231c7d0fcbb3
gitaunthama <gitaunthama@gmail.com>
2024-03-25 18:24:57 +0300

    complete work

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+  
+    if (win) {
+        return win.year;
+    } else {
+       
+        return undefined;
+    }
+}
+
+const records = [
+    { year: 2015, 
+    result: 'W' },
+    
+];
+
+console.log(superbowlWin( 'Denver Broncos' + records)); 
